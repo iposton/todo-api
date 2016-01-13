f2d8dcfe1d9ac9cf1f49d923d0481f5ca1e90520
iposton <ianposton@sbcglobal.net>
2016-01-12 18:48:25 -0800

    add variable playground demo

---
+++ variable-playground.js
@@ -0,0 +1,31 @@
+// var person = {
+// 	name: "Andrew",
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: "Andrew",
+// 	// 	age: 24
+// 	// };
+
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15, 28];
+
+function addGrades (grades) {
+	grades.push(44);
+	debugger;
+
+	
+
+}
+
+addGrades(grades);
+console.log(grades);
+
+
