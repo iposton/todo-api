bfc4f8873d250c140415b5d6a7b5565f73408bbd
iposton <ianposton@sbcglobal.net>
2016-01-12 16:30:55 -0800

    add PUT route to update api

--- server.js
+++ server.js
@@ -68,6 +68,38 @@ app.delete('/todos/:id', function (req, res) {
 	}
 });
 
+// Put 
+app.put('/todos/:id', function (req, res) {
+	// Here 
+  var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).json({"error": "not found"})
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).json({"error": "something bad happened. not completed."});
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		console.log('Update todo successful.');
+		validAttributes.description = body.description;
+
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).json({"error": "description not valid."});
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+
+});
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on '+ PORT +'!');
