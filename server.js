355967b8d8498aea444f5dfdce6a37a18985a46a
iposton <ianposton@sbcglobal.net>
2016-01-14 17:44:39 -0800

    convert GET todos to sequelize

--- server.js
+++ server.js
@@ -16,41 +16,49 @@ app.get('/', function(req, res) {
 
 // Get request all todos
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
 
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		});
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
 	}
 
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-		});
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
 	}
 
-	res.json(filteredTodos);
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
+		res.json(todos);
+	}, function(e) {
+		res.status(500).json({
+			'error': 'server error'
+		});
+	});
 });
 
 // Get request one todo
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
-  db.todo.findById(todoId).then(function(todo) {
-  	if (!!todo) {
-      res.json(todo.toJSON());
-  	} else {
-  		res.status(404).json({'error': 'there is no todo with the id of ' + todoId});
-  	}
-  }, function(e) {
-  	res.status(500).json({'error': 'server error'});
-  });
+	db.todo.findById(todoId).then(function(todo) {
+		if (!!todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).json({
+				'error': 'there is no todo with the id of ' + todoId
+			});
+		}
+	}, function(e) {
+		res.status(500).json({
+			'error': 'server error'
+		});
+	});
 
 	// var matchedTodo = _.findWhere(todos, {
 	// 	id: todoId
@@ -76,7 +84,7 @@ app.post('/todos', function(req, res) {
 
 	db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
-	}, function(e){
+	}, function(e) {
 		res.status(400).json(e);
 	});
 	// Validation
