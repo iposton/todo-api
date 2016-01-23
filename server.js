d04b4ae1271c3482d3311c5f1270596247a90f9b
iposton <ianposton@sbcglobal.net>
2016-01-22 16:54:10 -0800

    add login route and authenticate class method

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
-
+var bcrypt = require('bcrypt');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -93,7 +93,7 @@ app.delete('/todos/:id', function(req, res) {
 	}, function() {
 		res.status(500).json({
 			'error': 'server error'
-		})
+		});
 	});
 });
 
@@ -141,7 +141,17 @@ app.post('/users', function(req, res) {
 	});
 });
 
-db.sequelize.sync().then(function() {
+app.post('/users/login', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function(user) {
+		res.json(user.toPublicJSON());
+	}, function() {
+		res.status(401).json({'error': 'the credentials are incorrect'});
+	});
+});
+
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on ' + PORT + '!');
 	});
